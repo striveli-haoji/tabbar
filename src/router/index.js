b5ea62eb45097a8fcc3a6b7d344191789431090b
striveLi <2367025764@qq.com>
2020-12-06 19:00:42 +0800

    first commit

---
+++ src/router/index.js
@@ -0,0 +1,50 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+const Home = () => import('../views/home/Home.vue')
+const Category = () => import('../views/category/Category.vue')
+const Cart = () => import('../views/cart/Cart.vue')
+const Profile = () => import('../views/profile/Profile.vue')
+
+// 1、安装插件
+Vue.use(VueRouter)
+
+// vue-router 报错、:Avoided redundant navigation to current location 错误、路由重复
+const originalReplace = VueRouter.prototype.replace;
+VueRouter.prototype.replace = function replace(location) {
+  return originalReplace.call(this, location).catch(err => err);
+};
+
+// 2、创建路由器
+const routes = [
+  {
+    path: '',
+    redirect: '/home'
+  },
+  {
+    path: '/home',
+    component: Home
+  },
+  {
+    path: '/category',
+    component: Category
+  },
+  {
+    path: '/cart',
+    component: Cart
+  },
+  {
+    path: '/profile',
+    component: Profile
+  }
+]
+
+const router = new VueRouter({
+  routes,
+  mode: 'history'
+})
+
+// 3、导出router
+export default router
+
+
